7e5b4c938ffd80a97a74e353c2feecd69edd82e9
wangchengyuan <917515308@qq.com>
2018-12-01 18:32:23 +0800

    修改操作数据库相关代码，格式化代码

--- bin/datadeal.py
+++ bin/datadeal.py
@@ -2,61 +2,76 @@
 @author:wangchengyuan
 @file:datadeal.py
 @time:2018/11/16下午10:18
-@desc:
+@desc:操作数据库、跳板机操作数据库
 '''
 
 import pymysql
 from sshtunnel import SSHTunnelForwarder
+from bin import readConfig
 import paramiko
+
 
 class DataDeal():
 
-    #初始化读取配合文件
+    # 初始化读取配合文件
     def __init__(self):
-        print()
+        self.rdf = readConfig.ReadConfig()
 
-    #通过ssh方式访问数据库
-    def dboverssh(self):
-        with SSHTunnelForwarder(ssh_address_or_host='120.26.54.37',ssh_port=22,ssh_username='wangchengyuan',ssh_password='tianyalieren89') as tunnel:
-            conn=pymysql.connect(
-                host='',
-                port='',
-                user='',
-                passwd='',
-                db='',
+    # 通过ssh方式访问数据库
+    def db_overssh(self, sql):
+        with SSHTunnelForwarder(ssh_address_or_host=self.rdf.get_sshrelate('hostname'),
+                                ssh_port=22,
+                                ssh_username=self.rdf.get_sshrelate('username'),
+                                ssh_password=self.rdf.get_sshrelate('password')) as tunnel:
+            conn = pymysql.connect(
+                host=self.rdf.get_database('host'),
+                port=tunnel.local_bind_port,
+                user=self.rdf.get_database('user'),
+                passwd=self.rdf.get_database('passwd'),
+                db='quxuexi',
                 charset='utf8',
             )
-            cur=conn.cursor()
+            cur = conn.cursor()
             try:
-                cur.execute("")
+                cur.execute(sql)
+                data = cur.fetchall()
             finally:
                 cur.close()
                 conn.commit()
                 conn.close()
+            return data
 
-    #直接访问数据库
-    def dbread(self):
-        conn=pymysql.connect(
-            host='',
-            port='',
-            user='',
-            passwd='',
-            db='',
+    # 直接访问数据库
+    def db_read(self, sql):
+        conn = pymysql.connect(
+            host=self.rdf.get_database('host'),
+            port=3306,
+            user=self.rdf.get_database('user'),
+            passwd=self.rdf.get_database('passwd'),
+            db='quxuexi',
             charset='utf8'
         )
-        cur=conn.cursor()
+        cur = conn.cursor()
         try:
-            cur.execute()
+            cur.execute(sql)
+            data = cur.fetchall()
         finally:
             cur.close()
             conn.commit()
             conn.close()
 
+        return data
 
-    #
-    
+    # 查询数据
+    def db_select(self, sql, ssh=True):
+        if (ssh):
+            data = self.db_overssh(sql)
+        else:
+            data = self.db_read(sql)
 
+        return data
 
-
-
-
+    # 查询数据
+    def db_select(self, sql):
+        data = self.db_read(sql)
+        return data
--- bin/readConfig.py
+++ bin/readConfig.py
@@ -8,24 +8,21 @@
 import os
 import configparser
 
-#定义获取配置文件的路径
+# 定义获取配置文件的路径
 proDir = os.path.dirname(os.path.dirname(__file__))
-configDir = os.path.join(proDir,'config/config.ini')
+configDir = os.path.join(proDir, 'config/config.ini')
+
 
 class ReadConfig():
     def __init__(self):
         self.cf = configparser.ConfigParser()
         self.cf.read(configDir)
 
-    #增加获取对应配置的方法，获取 DATABASE、SSHRELATE，如有新配置，新增方法即可
-    def get_database(self,name):
-        value = self.cf.get('DATABASE',name)
+    # 增加获取对应配置的方法，获取 DATABASE、SSHRELATE，如有新配置，新增方法即可
+    def get_database(self, name):
+        value = self.cf.get('DATABASE', name)
         return value
 
-    def get_email(self,name):
-        value = self.cf.get('SSHRELATE',name)
+    def get_sshrelate(self, name):
+        value = self.cf.get('SSHRELATE', name)
         return value
-
-
-
-
