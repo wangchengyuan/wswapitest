b6bbe23d563849b2eb090321041d95c8380c0b45
wangchengyuan <917515308@qq.com>
2019-02-21 00:38:29 +0800

    修改sqlalchemy生成models文件

--- sqldao/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-@author:wangchengyuan
-@file:__init__.py.py
-@time:2019/1/30下午6:57
-@desc:
-'''
--- sqldao/sqlacodegentest.py
+++ sqldao/sqlacodegentest.py
@@ -9,19 +9,27 @@ import os
 from bin import readConfig
 
 proDir=os.path.dirname(__file__)
-print(proDir)
 
-def sqlacodegenTest(tablename):
+def sqlacodegenTest(tablename,dbname):
     readconf = readConfig.ReadConfig()
     filename = tablename + '.py'
     host = readconf.get_database('host')
     user = readconf.get_database('user')
     passwd = readconf.get_database('passwd')
-    db = readconf.get_database('db')
+    db = dbname
     dbaddress = 'mysql+pymysql://{}:{}@{}/{}'.format(user, passwd, host, db)
-    lines = 'sqlacodegen --tables %s --outfile %s ' % (tablename, filename) + dbaddress
-    print(lines)
-    if os.path.exists(filename):
+    dirpath=os.path.join(proDir,'modelfiles','%s') %db
+    #判断包是否存在，不存创建包
+    if os.path.exists(dirpath)&os.path.isdir(dirpath):
+        print("文件目录存在")
+    else:
+        print("文件目录不存在")
+        os.mkdir(dirpath)
+        with open(os.path.join(dirpath,'__init__.py'),'w'):
+            print("创建包成功")
+    #判断文件是否存在，不存在在对应路径下生成model文件
+    lines = 'sqlacodegen --tables %s --outfile %s ' % (tablename, os.path.join(dirpath,filename)) + dbaddress
+    if os.path.exists(os.path.join(dirpath,filename)):
         print('文件已存在')
     else:
         print('文件不存在')
@@ -29,4 +37,6 @@ def sqlacodegenTest(tablename):
 
 
 if __name__ == '__main__':
-    sqlacodegenTest('users')
+    dbname='auto_account'  #表所在数据库名称
+    tablename='users'      #表名称
+    sqlacodegenTest(tablename,dbname)
